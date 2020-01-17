31efe0ee09835040375707c41b76f8844f3692e7
Terence Stone <terence.stone25@gmail.com>
2020-01-16 22:43:36 -0500

    updates to index, contoller and models before deploy

--- controllers.py
+++ controllers.py
@@ -86,13 +86,16 @@ def update():
 ##admin routes##
 #admin dashboard
 def admin():
+    if 'admin_id' not in session:
+        return redirect('/')
     return render_template('admindash.html')
 
 def pi():
     return render_template('login.html')
 
 def admin_in():
-    
+    if 'admin_id' not in session:
+        return redirect('/')
     return redirect('/admindash')
 
 #process registration form
