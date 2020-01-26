d97733462af08a765f0789ac234de04a8d5f68d6
Terence Stone <terence.stone25@gmail.com>
2020-01-25 22:55:33 -0500

    updated controllers

--- controllers.py
+++ controllers.py
@@ -88,26 +88,3 @@ def update():
         update_parent = Parent.edit_parent(user_id, request.form)
         user = User.get(user_id)
     return redirect('/dashboard')
-
-##admin routes##
-#admin dashboard
-def admin():
-    if 'admin_id' not in session:
-        return redirect('/')
-    return render_template('admindash.html')
-
-def pi():
-    return render_template('login.html')
-
-def admin_in():
-    if 'admin_id' not in session:
-        return redirect('/')
-    return redirect('/admindash')
-
-#pull all records
-
-#logout user
-def logout():
-    session.clear()
-    return redirect('/')
- 
