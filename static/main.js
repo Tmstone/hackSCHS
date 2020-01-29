7e0f1f063f81abb87f6e63644ee51492cead71d9
Terence Stone <terence.stone25@gmail.com>
2020-01-28 23:39:15 -0500

    updating js validations on index page.

--- static/main.js
+++ static/main.js
@@ -9,7 +9,25 @@ $(document).ready(function(){
     $('loginForm').submit(
         function(event) {
            var isValid = True;
-            
+           //add email validation here
+
+           
+           var password = $("#password").val().trim()
+            var passwordPattern = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$';
+            if (password == "") {
+                $('#password').next().text("This field is required.")
+            }
+            else if (!passwordPattern.test(password)) {
+                $("#password").next().text('* Please enter a valid password: 6-20 characters, A-Z and (# $ % @ &)');
+                isValid = False;
+            } 
+            else {
+                ('#password').next().text("");
+            }
+            ('#password').val(password);
+            if (isValid == false) {
+             event.preventDefault();   
+            }
         }
     );
 });
