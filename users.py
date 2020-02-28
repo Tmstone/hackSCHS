e466db4fe8e90894e12353094678ff78c4892d48
Terence Stone <terence.stone25@gmail.com>
2020-02-27 22:03:02 -0500

    Added a counter to user.py

--- users.py
+++ users.py
@@ -1,13 +1,16 @@
 from models import User, Parent, db
 users = User.get_all_users()
+counter = 0
 for user in users:
-    print("Name: " + user.first_name + " " + user.last_name)
+    counter = counter + 1
+    print(str(counter) + " Name: " + user.first_name + " " + user.last_name)
     print("Email: " + user.email)
 print()
 print()
 contacts = Parent.get_all()
 for contact in contacts:
-    print("Parent Name: " + contact.parent_first + " " + contact.parent_last)
+    counter = counter + 1
+    print(str(counter) + " Parent Name: " + contact.parent_first + " " + contact.parent_last)
     print("Parent Email: " + contact.parent_email + " " + "Parent Phone: " + contact.parent_phone)
 
    
