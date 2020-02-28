11debeb20202f890e3d9aca692a879ae1ec37d69
Terence Stone <terence.stone25@gmail.com>
2020-02-27 22:13:34 -0500

    updating counters in user.py

--- users.py
+++ users.py
@@ -1,6 +1,8 @@
 from models import User, Parent, db
 users = User.get_all_users()
 counter = 0
+parent_counter = 0
+print("Attendees")
 for user in users:
     counter = counter + 1
     print(str(counter) + " Name: " + user.first_name + " " + user.last_name)
@@ -8,8 +10,10 @@ for user in users:
 print()
 print()
 contacts = Parent.get_all()
+print("Parent Contact")
 for contact in contacts:
-    print(str(counter) + " Parent Name: " + contact.parent_first + " " + contact.parent_last)
+    parent_counter = parent_counter + 1
+    print(str(parent_counter) + " Parent Name: " + contact.parent_first + " " + contact.parent_last)
     print("Parent Email: " + contact.parent_email + " " + "Parent Phone: " + contact.parent_phone)
 
    
